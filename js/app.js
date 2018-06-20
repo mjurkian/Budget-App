7a0c4890768a5e478e1899bea5a22c071365f616
EMDZEJ\mj <michaljurkian@gmail.com>
2018-06-20 13:05:02 +0100

    keypress

--- js/app.js
+++ js/app.js
@@ -20,7 +20,8 @@ var UIController = (function() {
 
 var Controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
+    var ctrlAddItem = function () {
+            
         // 1. Get the field input data
 
         // 2. Add the item to the budget controller
@@ -30,6 +31,17 @@ var Controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
+
+        console.log('it works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
 
 })(budgetController, UIController);
