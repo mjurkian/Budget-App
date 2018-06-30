253d4e3372db773bedd554d9e3a98cdda886b6d5
EMDZEJ\mj <michaljurkian@gmail.com>
2018-06-30 23:36:13 +0100

    Create HTML string with placeholder text

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // BUDGET
-var budgetController = (function() {
-    
-    var Expense = function(id, description, value) {
+var budgetController = (function () {
+
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-    var Income = function(id, description, value) {
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
@@ -25,7 +25,7 @@ var budgetController = (function() {
     };
 
     return {
-        addItem: function(type, des, val) {
+        addItem: function (type, des, val) {
             var newItem, ID;
 
             // [1 2 3 4 5], next ID = 6
@@ -34,7 +34,7 @@ var budgetController = (function() {
 
             // Create new ID
             if (data.allItems[type].length > 0) {
-            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
@@ -53,7 +53,7 @@ var budgetController = (function() {
             return newItem;
         },
 
-        testing: function() {
+        testing: function () {
             console.log(data);
         }
     };
@@ -65,7 +65,7 @@ var budgetController = (function() {
 
 // UI 
 
-var UIController = (function() {
+var UIController = (function () {
 
     var DOMstrings = {
         inputType: '.add__type',
@@ -75,7 +75,7 @@ var UIController = (function() {
     };
 
     return {
-        getInput: function(){
+        getInput: function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
@@ -83,7 +83,31 @@ var UIController = (function() {
             };
         },
 
-        getDOMstrings: function() {
+        addListItem: function (obj, type){
+            var html, 
+                newHtml, 
+                element;
+
+            // Create HTML string with placeholder text
+            
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+            
+            // insert the HTML into the DOM
+        },
+
+        getDOMstrings: function () {
             return DOMstrings;
         }
 
@@ -95,15 +119,15 @@ var UIController = (function() {
 
 // Controller
 
-var Controller = (function(budgetCtrl, UICtrl) {
+var Controller = (function (budgetCtrl, UICtrl) {
 
     var setupEventListeners = function () {
         var DOM = UICtrl.getDOMstrings();
 
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-        document.addEventListener('keypress', function(event){
-            
+        document.addEventListener('keypress', function (event) {
+
             if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
@@ -113,10 +137,10 @@ var Controller = (function(budgetCtrl, UICtrl) {
 
     var ctrlAddItem = function () {
         var input, newItem;
-            
+
         // 1. Get the field input data
         var input = UICtrl.getInput();
-    
+
         // 2. Add the item to the budget controller
         var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -129,7 +153,7 @@ var Controller = (function(budgetCtrl, UICtrl) {
     };
 
     return {
-        init: function() {
+        init: function () {
             console.log('Applicetion has starder');
             setupEventListeners();
         }
