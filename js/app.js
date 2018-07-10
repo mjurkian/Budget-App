0d668f6bc7d6f6738131fb0886f4446dfd098b15
EMDZEJ\mj <michaljurkian@gmail.com>
2018-07-10 18:01:40 +0100

    Clear the fields and focus

--- js/app.js
+++ js/app.js
@@ -112,6 +112,20 @@ var UIController = (function () {
         
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -152,6 +166,10 @@ var Controller = (function (budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
 
         UICtrl.addListItem(newItem, input.type);
+
+        // 3.5 Clear the fields
+
+        UICtrl.clearFields();
 
         // 4. Calculate the budget
 
